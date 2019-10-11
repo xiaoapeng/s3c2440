94306abfd0d9a680ffbce2077165f98c8b55742a
xiaoapeng <1336386533@qq.com>
2019-10-10 18:19:34 -0700

    add 1.c

---
+++ 1.c
@@ -0,0 +1,18 @@
+eth0      Link encap:Ethernet  HWaddr 00:0c:29:6b:6b:12  
+          inet addr:192.168.1.4  Bcast:192.168.1.255  Mask:255.255.255.0
+          inet6 addr: fe80::20c:29ff:fe6b:6b12/64 Scope:Link
+          UP BROADCAST RUNNING MULTICAST  MTU:1500  Metric:1
+          RX packets:1552 errors:0 dropped:0 overruns:0 frame:0
+          TX packets:714 errors:0 dropped:0 overruns:0 carrier:0
+          collisions:0 txqueuelen:1000 
+          RX bytes:228326 (228.3 KB)  TX bytes:84934 (84.9 KB)
+
+lo        Link encap:Local Loopback  
+          inet addr:127.0.0.1  Mask:255.0.0.0
+          inet6 addr: ::1/128 Scope:Host
+          UP LOOPBACK RUNNING  MTU:16436  Metric:1
+          RX packets:8 errors:0 dropped:0 overruns:0 frame:0
+          TX packets:8 errors:0 dropped:0 overruns:0 carrier:0
+          collisions:0 txqueuelen:0 
+          RX bytes:500 (500.0 B)  TX bytes:500 (500.0 B)
+
